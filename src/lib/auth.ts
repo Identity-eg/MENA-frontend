ee9fefd785826a469206f4d024bf4c8bddad87da
hTaw99 <hady.tawfik1999@gmail.com>
2026-02-27 17:55:16 +0200

    refactor: use constants for cookie names in clearServerCredentials function to improve maintainability

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
 import { createServerFn } from '@tanstack/react-start'
 import { deleteCookie } from '@tanstack/react-start/server'
+import { ACCESS_TOKEN_NAME, REFRESH_TOKEN_NAME } from '@/constants/auth'
 
 export const clearServerCredentials = createServerFn().handler(async () => {
-  deleteCookie('refreshToken')
-  deleteCookie('accessToken')
+  deleteCookie(REFRESH_TOKEN_NAME)
+  deleteCookie(ACCESS_TOKEN_NAME)
 })
